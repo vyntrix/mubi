082e6f5082d8ab22fa6f1dc5da8f5ac69a2c16a3
Vyntrix <frisch_valentin@icloud.com>
2025-01-04 13:31:01 +0900

    chore: removed bento area

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,23 +8,3 @@
         <a href="https://github.com/vyntrix/mubi" target="_blank" class="btn-sec"><div>✨</div>Star on GitHub</a>
     </div>
 </section>
-
-<!--<section class="grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
-    <div class="card relative lg:row-span-2 justify-evenly">
-        <h2 class="text-xl font-bold">How are you feeling?</h2>
-        <div class="hbox text-4xl">
-            <div class="i-mingcute-happy-fill"></div>
-            <div class="i-mingcute-angry-fill"></div>
-            <div class="i-mingcute-sob-fill"></div>
-        </div>
-    </div>
-    <div class="card relative max-lg:row-start-1">
-
-    </div>
-    <div class="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2 card"></div>
-    <div class="relative lg:row-span-2 card-pri">
-        <h2 class="text-xl font-bold">Write about your day.</h2>
-        <input class="bg-light-5 p-2 w-full rounded-lg border" disabled placeholder="Write here..."/>
-    </div>
-</section>
--->
