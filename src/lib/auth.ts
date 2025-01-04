1f6208d9cf7e1a3444ab1223625e191b8df6413a
Vyntrix <frisch_valentin@icloud.com>
2025-01-04 13:50:34 +0900

    chore: setup prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,8 @@
+import { prisma } from '$lib/server/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import { SvelteKitAuth } from '@auth/sveltekit'
 
-export const { handle } = SvelteKitAuth({
+export const { handle, signIn, signOut } = SvelteKitAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [],
 })
---
+++ src/lib/server/prisma.ts
@@ -0,0 +1,9 @@
+import { dev } from '$app/environment'
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (dev)
+  globalForPrisma.prisma = prisma
