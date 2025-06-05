242bf8a569fd61ec43524235c8ae9cc1a06c4015
vyntrix <72789695+Vyntrix@users.noreply.github.com>
2025-06-05 12:30:06 +0200

    chore: service worker test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,27 @@
   import Footer from '$lib/components/footer.svelte';
 
   import * as config from '$lib/config'
+  import { onMount } from 'svelte';
+
+  async function detectSWUpdate() {
+    const registration = await navigator.serviceWorker.ready
+
+    registration.addEventListener('updatefound', () => {
+        const newSW = registration.installing
+        newSW?.addEventListener('statechange', () => {
+            if (newSW.state === 'installed') {
+                if (confirm('New update available! Reload to update?')) {
+                    newSW.postMessage({ type: 'SKIP_WAITING' })
+                    window.location.reload()
+                }
+            }
+        })
+    })
+  }
+
+  onMount(() => {
+    detectSWUpdate()
+  })
 
   let { children } = $props()
 </script>
